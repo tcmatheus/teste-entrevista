6773ec54b136f84c1c912808872e60f867cb6775
tcmatheus <matheustcampos1904@gmail.com>
2024-06-17 11:49:02 -0300

    add github

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function home() {
+    return (
+        <View >
+            <text> testandoaccd</text>
+        </View>
+    );
+}
+
